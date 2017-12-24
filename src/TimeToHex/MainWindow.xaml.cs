9d3248d3c91ca94b24819a686a57254c1d5373d2
peach <skpikao233@gmail.com>
2017-12-24 23:20:24 +0000

    Add source

---
+++ src/TimeToHex/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeToHex
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Timer updateTimer = new Timer();
+            updateTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            updateTimer.Interval = 1000;
+            updateTimer.Enabled = true;
+        }
+
+        private void Update()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                hourDisplay.Text = DateTime.Now.Hour.ToString();
+                minuteDisplay.Text = DateTime.Now.Minute.ToString();
+                secondDisplay.Text = DateTime.Now.Second.ToString();
+                BrushConverter bc = new BrushConverter();
+                try
+                {
+                    this.Background = (Brush)bc.ConvertFrom(Time2Hex.GetColourFromTime());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Exception caught!");
+                }
+            });
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Update();
+        }
+
+        private void Window_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/TimeToHex/Time2Hex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeToHex
+{
+    class Time2Hex
+    {
+        public static int TimeScale = 1;
+
+        public static string GetColourFromTime()
+        {
+            string seconds;
+            string minutes;
+            string hours;
+            if (DateTime.Now.Second.ToString().Length == 1)
+            {
+                seconds = "0" + DateTime.Now.Second.ToString();
+            }
+            else { seconds = DateTime.Now.Second.ToString(); }
+
+            if (DateTime.Now.Minute.ToString().Length == 1)
+            {
+                minutes = "0" + DateTime.Now.Minute.ToString();
+            }
+            else { minutes = DateTime.Now.Minute.ToString(); }
+
+            if (DateTime.Now.Hour.ToString().Length == 1)
+            {
+                hours = "0" + DateTime.Now.Hour.ToString();
+            }
+            else { hours = DateTime.Now.Hour.ToString(); }
+
+            return "#FF" + hours + minutes + seconds;
+        }
+    }
+}
